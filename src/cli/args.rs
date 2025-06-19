197622b23fbc94475640db3f3c06320584aa00d7
moonyango <mosesadrian825@gmail.com>
2025-06-20 02:40:37 +0300

    add output option to CLI for specifying file name

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,10 +1,19 @@
 use clap::{Parser, ValueEnum};
-use std::path::PathBuf; 
+use std::path::PathBuf;
 
 #[derive(Parser, Debug, Clone)]
 #[command(name = "wget", about = "A simple wget clone", version = "0.1.0")]
 pub struct Cli {
-    // URL(s) to download
+    /// URL(s) to download
     #[arg(required = true, help = "URL(s) to download")]
     pub urls: Vec<String>,
+
+    /// Save file as a specific name (-O)
+    #[arg(
+        short = 'O',
+        long,
+        help = "Write documents to file",
+        conflicts_with = "directory_prefix"
+    )]
+    pub output: Option<PathBuf>,
 }
