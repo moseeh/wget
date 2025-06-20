12cf7769c7a91e79b06f431b267b48580f72fae7
moonyango <mosesadrian825@gmail.com>
2025-06-20 03:31:13 +0300

    feat: simple main implementation

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, ValueEnum};
+use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug, Clone)]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let args = cli::Cli::parse();
+    if let Err(e) = args.validate() {
+        eprintln!("Argument error: {}", e);
+        std::process::exit(1);
+    }
+    println!("CLI parsed and validated successfully!");
+    println!("{:#?}", args);
 }
